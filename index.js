30c9bea9bce2542548a040b40c2eefeeb24df2f6
Davinder Kumar <abhiverma97819@gmail.com>
2025-07-20 19:54:26 +0530

    add bullmq

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const express = require('express')
 const cors = require('cors')
 const multer = require('multer')
+const { Queue } = require('bullmq');
+
+const queue = new Queue('file-upload')
 
 const storage = multer.diskStorage({
     destination: function (req, file, cb) {
@@ -21,7 +24,12 @@ app.get('/', (req, res) => {
     return res.json({ status: 'All Good!' })
 })
 
-app.post('/upload/pdf', upload.single('pdf'), (req, res) => {
+app.post('/upload/pdf', upload.single('pdf'), async (req, res) => {
+    await queue.add('file-ready', JSON.stringify({
+        filename: req.file.originalname,
+        destination: req.file.destination,
+        path: req.file.path
+    }))
     return res.json({ message: 'uploaded' })
 })
 
