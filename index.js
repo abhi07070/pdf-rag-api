0ab8f58c787686035176b792a1e1c76f325a437e
Davinder Kumar <abhiverma97819@gmail.com>
2025-07-20 18:41:44 +0530

    implement multer middleware funtionality to upload pdf

---
+++ index.js
@@ -0,0 +1,28 @@
+const express = require('express')
+const cors = require('cors')
+const multer = require('multer')
+
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, 'uploads/');
+    },
+    filename: function (req, file, cb) {
+        const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1e9);
+        cb(null, `${uniqueSuffix}-${file.originalname}`);
+    },
+});
+
+const upload = multer({ storage: storage })
+
+const app = express()
+app.use(cors())
+
+app.get('/', (req, res) => {
+    return res.json({ status: 'All Good!' })
+})
+
+app.post('/upload/pdf', upload.single('pdf'), (req, res) => {
+    return res.json({ message: 'uploaded' })
+})
+
+app.listen(8000, () => console.log(`server started on PORT: ${8000}`))
